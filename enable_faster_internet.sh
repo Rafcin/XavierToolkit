be4cac85bfb78cb00a37914a06b1881bdc97999a
Rafcin <Rafcin.s@gmail.com>
2018-10-22 00:06:20 +0000

    edit

--- enable_faster_internet.sh
+++ enable_faster_internet.sh
@@ -2,6 +2,7 @@
 
 #Enables larger packet transfer on the Xavier.
 #Set to mode 8
+#10/21/16
 
 echo "Setting iwlwifi options to mode 8 "
 echo "options iwlwifi 11n_disable=8" | sudo tee -a /etc/modprobe.d/iwlwifi.conf
